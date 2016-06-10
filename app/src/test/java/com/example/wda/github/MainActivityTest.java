1c407cbec6db9f8541f07e6857dbe642711dde24
Wojciech Dawiskiba <wojciech.dawiskiba@blstream.com>
2016-06-10 16:53:45 +0200

    base login interface added

--- app/src/test/java/com/example/wda/github/MainActivityTest.java
+++ app/src/test/java/com/example/wda/github/MainActivityTest.java
@@ -8,6 +8,10 @@ import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.annotation.Config;
 import org.robolectric.util.ActivityController;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 import static org.junit.Assert.*;
 
 /**
@@ -30,12 +34,33 @@ public class MainActivityTest {
     }
 
     @Test
-    public void activityStarted() throws Exception{
-        assertNotNull(activityController.create().start().get());
+    public void activtyHasButtonWithLoginText(){
+        MainActivity activity = getVisibleActivty();
+        Button button= (Button) activity.findViewById(R.id.login_button);
+
+        assertEquals(View.VISIBLE,button.getVisibility());
+        assertEquals("Login",button.getText());
     }
 
     @Test
-    public void activityResumed() throws Exception{
-        assertNotNull(activityController.create().start().resume().visible().get());
+    public void activtyHasLoginEditTextWithLoginHint(){
+        MainActivity activity = getVisibleActivty();
+        EditText editText= (EditText) activity.findViewById(R.id.login_input );
+
+        assertEquals(View.VISIBLE,editText.getVisibility());
+        assertEquals("login",editText.getHint());
+    }
+
+    @Test
+    public void activityHasPasswordEditTestWithPasswordHint(){
+        MainActivity activity=getVisibleActivty();
+        EditText editText= (EditText) activity.findViewById(R.id.password_input);
+
+        assertEquals(View.VISIBLE,editText.getVisibility());
+        assertEquals("password",editText.getHint());
+    }
+
+    private MainActivity getVisibleActivty() {
+        return activityController.create().start().resume().visible().get();
     }
 }
